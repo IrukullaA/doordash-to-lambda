7f1250bcc02622b9c9ace5b18a5f5c5048e8325f
Abhinay Irukulla <irukulla.dba@gmail.com>
2025-01-30 16:51:41 -0800

    Building cicd data extraction for doordash

---
+++ lambda_function.py
@@ -0,0 +1,48 @@
+import json
+import pandas as pd
+import boto3
+import os
+import io
+from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client('s3')
+
+def lambda_handler(event,context):
+    #Source bucket and object key
+    source_bucket = event['Records'][0]['s3']['bucket']['name']
+    source_key = event['Records'][0]['s3']['bucket']['key']
+    #read file from s3 source
+    obj = s3.get_object(Bucket = source_bucket, Key = source_key)
+    body = obj['Body'].read()
+    json_dicts = body.decode('utf-8').split('\r\n')
+    df = pd.DataFrame(columns = ['id','status','amount','date'])
+
+    for line in json_dicts:
+        py_dict = json.loads(line)
+        if py_dict['status'] == 'delivered':
+            df.loc[py_dict['id']] = py_dict
+
+    df.to_csv('/tmp/test.csv',sep = ',')
+    print('test.csv file created')
+
+    try:
+        date_var = str(date.today())
+        file_name = 'processed_data/{}_processed_data.csv'.format(date_var)
+    except:
+        file_name = 'processed_data/processed_data.csv'
+    lambda_path = '/tmp/test.csv'
+    bucket_name = os.getenv('destination_bucket')
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    bucket.upload_file('/tmp/test.csv',file_name)
+
+    #sns to deliver file processed request
+    sns = boto3.client('sns')
+    response = sns.publish(
+            TopicArn=os.getenv('TopicArn'),
+            Message = "File {} has been formatted and filtered. Its been stored in {} as {}".format(source_key,bucket_name,file_name)
+            )
